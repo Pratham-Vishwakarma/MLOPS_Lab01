8644bdf52d287fa14fc4bb87a306e1e720e56ad8
Pratham-Vishwakarma <pratham.vishwakarma125940@gmail.com>
2026-04-16 21:56:46 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+X = np.array([[1],[2],[3],[4]])
+y = np.array([2,4,6,8])
+
+model = LinearRegression()
+model.fit(X,y)
+
+prediction = model.predict([[5]])
+
+print("Prediction:", prediction[0])
